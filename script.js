44e6f51e319d7901c04dbb5f75ef01ac8313301e
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-24 16:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let value = document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
-
+// alert("in")
+	value.forEach((data)=>{
+		data.style.color="rgb(0, 128, 0)"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
+// alert("move out")
 
-    
+	value.forEach((data)=>{
+		data.style.color="rgb(0,0, 0)"
+	})
 }
